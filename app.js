34b2a8d50a4333955281165ac8c31c38dbba0677
Vijeesh301 <93731675+Vijeesh301@users.noreply.github.com>
2022-01-09 21:49:08 +0530

    second

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require ('body-parser');
 const nodemailer = require ('nodemailer');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.engine ('handlebars', exphbs);
 // app.set ('view engine', 'handlebars');
